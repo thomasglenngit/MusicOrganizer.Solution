964da2124081f4923282089da847f44346acbb71
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-27 12:03:01 -0700

    change Record class for connecting to database and get all records
    co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>

--- MusicOrganizer/Models/Record.cs
+++ MusicOrganizer/Models/Record.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace MusicOrganizer.Models
 {
@@ -7,29 +8,53 @@ namespace MusicOrganizer.Models
     public string Title { get; set; }
     public string Artist { get; set; }
     public int Id { get; }
-    private static List<Record> _records = new List<Record> { };
 
     public Record(string title, string artist)
     {
       Title = title;
       Artist = artist;
-      _records.Add(this);
-      Id = _records.Count;
+    }
+
+    public Record(int id, string title, string artist)
+    {
+      Id = id;
+      Title = title;
+      Artist = artist;
     }
 
     public static List<Record> GetAll()
     {
-      return _records;
+      List<Record> allRecords = new List<Record> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM records;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+        int recordId = rdr.GetInt32(0);
+        string recordTitle = rdr.GetString(1);
+        string recordArtist = rdr.GetString(2);
+        Record newRecord = new Record(recordId, recordTitle, recordArtist);
+        allRecords.Add(newRecord);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allRecords;
     }
 
     public static void ClearAll()
     {
-      _records.Clear();
+     
     }
 
     public static Record Find(int searchId)
     {
-      return _records[searchId - 1];
+      Record placeholderRecord = new Record("placeholder Title", "placeholder Artist");
+      return placeholderRecord;
     }
   }
 }
