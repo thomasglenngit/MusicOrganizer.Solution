a6252ee59ad7ad9d313d1017690bd5f696b0992a
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-27 13:56:43 -0700

    create Tests files to test functionality of Models and database connection
    co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>

---
+++ MusicOrganizer.Tests/ModelTests/RecordTests.cs
@@ -0,0 +1,84 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using MusicOrganizer.Models;
+using System;
+using MySql.Data.MySqlClient;
+
+namespace MusicOrganizer.Tests
+{
+  [TestClass]
+  public class RecordTest : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Record.ClearAll();
+    }
+
+    public RecordTest()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=music_organizer_test;";
+    }
+
+    [TestMethod]
+    public void RecordConstructor_CreatesInstanceOfRecord_Record()
+    {
+      Record  newRecord = new Record("title", "artist");
+      Assert.AreEqual(typeof(Record), newRecord.GetType());
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      // Arrange
+      List<Item> newList = new List<Item> { };
+
+      // Act
+      List<Item> result = Item.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      //Act
+      List<Item> result = Item.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      int result = newItem.Id;
+
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+
+      Item result = Item.Find(2);
+
+      Assert.AreEqual(newItem2, result);
+    }
+  }
+}
